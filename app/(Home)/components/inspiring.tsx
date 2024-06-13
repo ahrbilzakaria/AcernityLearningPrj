0deda74e84ba4b3db56839fd90f3d6cda46ba3bf
ahrbilzakaria <zakariaahr@gmail.com>
2024-06-13 22:47:50 +0100

    finals

--- app/(Home)/components/inspiring.tsx
+++ app/(Home)/components/inspiring.tsx
@@ -3,14 +3,11 @@ import { motion } from "framer-motion";
 import { WavyBackground } from "@/components/wavy-background";
 import { FlipWords } from "@/components/flip-words";
 
-export default function Ispiring() {
+export default function Inspiring() {
   const words = ["Better", "Cute", "Amazing", "Modern"];
   return (
-    <div className="overflow-hidden">
-      <WavyBackground className="max-w-4xl mx-auto flex justify-center items-center flex-col overflow-hidden  pb-40">
-        {/* <p className=" text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
-          Hero waves are cool
-        </p> */}
+    <div className="overflow-hidden h-[80%]  ">
+      <WavyBackground className="max-w-4xl mx-auto max-h-screen flex justify-center items-center flex-col overflow-hidden   pb-40">
         <div className="flex items-center text-6xl font-bold">
           Build
           <FlipWords words={words} />
--- app/(Home)/page.tsx
+++ app/(Home)/page.tsx
@@ -3,10 +3,13 @@ import NavBar from "./components/navBar";
 import Header from "./components/Header";
 import { BackgroundBeams } from "./components/background-beams";
 import Services from "./components/services";
-import Ispiring from "./components/ispiring";
+import Inspiring from "./components/inspiring";
+import { Projects } from "./components/Projects";
+import { WobbleCardDemo } from "./components/bento";
+
 export default function page() {
   return (
-    <div className="min-h-screen ">
+    <div>
       <div className="relative">
         <div className="w-[90%] h-screen mx-auto">
           <NavBar />
@@ -14,10 +17,14 @@ export default function page() {
         </div>
         <BackgroundBeams className=" absolute -z-10 " />
       </div>
-      <div className="w-[90%] h-screen mx-auto overflow-hidden ">
+      <div className="w-[90%] mx-auto  ">
         <Services />
-        <Ispiring />
-      </div>
+        <Inspiring />
+        <Projects />
+
+        <WobbleCardDemo />
+      </div>{" "}
+      <p className="text-center my-10">Made by zeus</p>
     </div>
   );
 }
